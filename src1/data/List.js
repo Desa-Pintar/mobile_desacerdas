f47fedbeffe04f877666ad4d34a5d6aa82c949f9
informastudios <informastudios@gmail.com>
2020-12-01 10:31:50 +0700

    'message'

---
+++ src1/data/List.js
@@ -0,0 +1,98 @@
+export const List = [
+  {
+    id: 1,
+    title: 'Surat Keterangan Domisili',
+    body: [
+      {
+        subjudul: '',
+        isi:
+          '-Membawa surat pengantar RT/RW \n-Membawa fotocopy Kartu Tanda Penduduk (KTP)\n-Membawa fotocopy bukti pelunasan PBB tahun berjalan',
+      },
+    ],
+  },
+  {
+    id: 2,
+    title: 'Surat Keterangan SKCK',
+    body: [
+      {
+        subjudul: '',
+        isi: '-Membawa fotocopy Kartu Keluarga\n-Membawa fotocopy KTP',
+      },
+    ],
+  },
+  {
+    id: 3,
+    title: 'Surat Keterangan Kelahiran',
+    body: [
+      {
+        subjudul: '',
+        isi:
+          '-Membawa Surat Pengantar RT\n-Membawa Kartu Keluarga (KK) dan fotocopy\n -Membawa KTP dan fotocopy \n-Membawa Surat Nikah dan fotocopy\n-Surat Keterangan dari Bidan/Rumah Sakit dan fotocopy\n-KTP pelapor dan fotocopy\n-KTP 2 orang saksi dan fotocopy',
+      },
+    ],
+  },
+  {
+    id: 4,
+    title: 'Akta Kelahiran',
+    body: [
+      {
+        subjudul: '',
+        isi:
+          '- Formulir/Surat Keterangan Kelahiran dari Desa/Kelurahan\n-Surat keterangan kelahiran dari Rumah Sakit/ Dokter/ Bidan Penlolong\n -Buku Nikah/ Akta Perkawinan Orang Tua lengkap dengan tandatangan pejabat yang berwenang\n-Kartu Keluarga\n-KTP-el Orang Tua\n-KTP-el Pelapor\n-KTP Saksi 2(dua) Orang\nUsia Kelahiran lebih Dari 60 hari ada Formulir/ Surat Dispensasi',
+      },
+    ],
+  },
+  {
+    id: 5,
+    title: 'Akta Kematian',
+    body: [
+      {
+        subjudul: '',
+        isi:
+          '- Formulir Pelaporan Kematian\n-Formulir/ Surat Keterangan Kematian dari Desa/Kelurahan\n -KTP-el yang meninggal/Jenazah\n (jika hilang, diganti dengan Surat Kehilangan KTP-el dari Kepolisian)\n-Kartu Keluarga\n-Surat Keterangan Kematian Dokter/Paramedis\n-KTP-el Pelapor\n-KTP Saksi 2(dua) Orang',
+      },
+    ],
+  },
+  {
+    id: 6,
+    title: 'Ktp Elektronik',
+    body: [
+      {
+        subjudul: 'KTP Rusak/Perubahan Elemen Data :',
+        isi:
+          '-Kartu Keluarga yang sudah ditandatangani\n-KTP Asli',
+      },
+      {
+        subjudul: 'KTP Kehilangan :',
+        isi:
+          '-Kartu Keluarga yang sudah ditandatangani\n-Surat Kehilangan dari Kepolisian',
+      },
+      {
+        subjudul: 'KTP Pemula :',
+        isi:
+          '-Kartu Keluarga yang sudah ditandatangani\n',
+      },
+    ],
+  },
+  {
+    id: 7,
+    title: 'KIA ( Kartu Identitas Anak )',
+    body: [
+      {
+        subjudul: 'KIA Baru :',
+        isi:
+          '-Kartu Keluarga yang sudah ditandatangani\n-Akta Kelahiran\n-Pas foto berwarna (utk usia anak > 5 tahun)\n',
+      },
+      {
+        subjudul: 'KIA Perpanjangan :',
+        isi:
+          '-KIA Asli yang sudah dipotong\n-Kartu Keluarga yang sudah ditandatangani\n-Pas foto berwarna\n',
+      },
+      {
+        subjudul: 'KIA Hilang :',
+        isi:
+          '-Surat Kehilangan dari Kepolisian\n-Kartu Keluarga yang sudah ditandatangani\n-Pas foto berwarna\n\n',
+      },
+    ],
+  },
+];
